3a4006dbb6057cf49e1e96883cc11afdaa40755a
LuisAlbertoQ <103071836+LuisAlbertoQ@users.noreply.github.com>
2024-07-05 04:38:12 -0500

    first commit

---
+++ websocketserver/src/main/java/com/chatsocket/websocketserver/controller/MensajeController.java
@@ -0,0 +1,17 @@
+package com.chatsocket.websocketserver.controller;
+
+import com.chatsocket.websocketserver.model.Mensaje;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MensajeController {
+
+    @MessageMapping("/envio")
+    @SendTo("/tema/mensajes")
+    public Mensaje envio(Mensaje mensaje){
+        return new Mensaje(mensaje.nombre(), mensaje.contenido());
+    }
+}
---
+++ websocketserver/src/main/java/com/chatsocket/websocketserver/model/Mensaje.java
@@ -0,0 +1,4 @@
+package com.chatsocket.websocketserver.model;
+
+public record Mensaje(String nombre, String contenido) {
+}
